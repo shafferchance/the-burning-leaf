16ee69303aedf4e7961b8366af6f6bad3211bcdf
Chanceler Shaffer <shafferchance@gmail.com>
2020-03-13 05:28:26 -0400

    Backend database configured, make some front facing code thoruhgout the day

---
+++ backend/logic/database-login.js
@@ -0,0 +1,28 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const AUTH = "DEFAULT"
+const DB_NAME = "cigar_store";
+const URL = 'localhost:27017'; // On local instance, may move to a config or env var soonish
+const USER = "reader";
+
+/**
+ * 
+ * @param {Object} [user = User] - User object containing username and password
+ * @param {String} [dbName = DB_NAME] - Database that the client will be connected to
+ * @param {String} [authMech = AUTH] - Mechanism to conduct authenication
+ * 
+ * @returns {Promise} Resolves with 2-tuple of datbase
+ */
+export const connect = (user = USER, dbName = DB_NAME, authMech = AUTH) => {
+    const client = new MongoClient(`mongodb://${encodeURIComponent(user)}:${encodeURIComponent(password)}@${url}?authMechanism=${authMech}`);
+    return new Promise((res, rej) => {
+        client.connect(err => {
+            if (err) 
+            {
+                rej(err);
+            }
+            console.log("Connected successfully to server");
+            res([client.db(dbName), client]); // Will return 2-tuple containing database and client connection
+        });
+    });
+}
