81ff54ca3750655c5be1b609b1d52af1ed3b647a
Chanceler Shaffer <shafferchance@gmail.com>
2020-03-13 22:54:17 -0400

    Frontend implementation started and most endpoints layed out awaiting to be filled with generic CRUD DB functions.

--- backend/controllers/general.controller.js
+++ backend/controllers/general.controller.js
@@ -1,5 +1,46 @@
 const general = require('express').Router();
 
+general.get("/announcements", (req, res, next) => {
+
+});
+
+general.post("/announcements", (req, res, next) => {
+
+});
+
+general.put("/announcements", (req, res, next) => {
+
+});
+
+general.delete("/announcements", (req, res, next_) => {
+
+});
+
+
+general.get("/contact", (req, res, next) => {
+
+});
+
+general.post("/contact", (req, res, next) => {
+
+});
+
+general.delete("/contact", (req, res, next) => {
+
+});
+
+general.get("/events", (req, res, next) => {
+
+});
+
+general.post("/events", (req, res, next) => {
+
+});
+
+general.delete("/events", (req, res, next) => {
+
+});
+
 general.get("/landing_pictures", (req, res, next) => {
 
 });
@@ -11,6 +52,5 @@ general.post("/landing_pictures", (req, res, next) => {
 general.delete("/landing_pictures", (req, res, next) => {
 
 });
-
 
 module.exports = general;
