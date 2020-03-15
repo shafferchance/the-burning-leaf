e387223dd8c678ebf08d381af6dc787591477bac
Chanceler Shaffer <shafferchance@gmail.com>
2020-03-14 23:56:46 -0400

    Deleting top level node_modules that was unused

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const about = require('./controllers/about.controller.js');
+const general = require('./controllers/general.controller.js');
+const inv = require('./controllers/inventory.controller.js');
+
 const app = express();
 
 app.use(express.static(path.resolve('..','frontend','public')));
+app.use('/about', about);
+app.use('/general', general);
+app.use('/inv', inv);
 
 app.listen(8000, () => console.log("Listening on port 8000"));
