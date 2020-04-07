49230b3fd83eb6d4def01bd207c6c70876818fa4
shafferchance <shafferchance@gmail.com>
2020-04-07 04:16:46 -0400

    correcting socials, adjusting icons, and placement of phone number as requested

--- backend/main.js
+++ backend/main.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://normie:W3c{}://!@cigar.temporaltech.app/?authSource=admin', {useNewUrlParser: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("Database connected");
-});
+// mongoose.connect('mongodb://normie:W3c{}://!@cigar.temporaltech.app/?authSource=admin', {useNewUrlParser: true});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+//     console.log("Database connected");
+// });
 
 const about = require('./controllers/about.controller.js');
 const general = require('./controllers/general.controller.js');
@@ -15,9 +15,9 @@ const inv = require('./controllers/inventory.controller.js');
 
 const app = express();
 
-app.use(express.static(path.resolve('..','frontend','public')));
-app.use('/about', about);
-app.use('/general', general);
-app.use('/inv', inv);
+app.use(express.static(path.resolve('..','static')));
+app.use('/api/v1/about', about);
+app.use('/api/v1/general', general);
+app.use('/api/v1/inv', inv);
 
 app.listen(8000, () => console.log("Listening on port 8000"));
